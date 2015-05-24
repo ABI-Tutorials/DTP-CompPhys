55caaa1555a9f95ee47f39c1ae81da598b4803fb
David Nickerson <nickerso@users.sourceforge.net>
2015-05-25 09:54:58 +1200

    change author and copyright to UoA

--- source/conf.py
+++ source/conf.py
@@ -49,8 +49,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DTP Computational Physiology'
-copyright = u'2015, Andre'
-author = u'Andre'
+copyright = u'2015, University of Auckland'
+author = u'University of Auckland'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -225,7 +225,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'DTPComputationalPhysiology.tex', u'DTP Computational Physiology Documentation',
-   u'Andre', 'manual'),
+   u'University of Auckland', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
